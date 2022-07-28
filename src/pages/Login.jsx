442f1e6e2bd6b63a9905a13af64b8e78377e3052
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-28 16:02:45 +0530

    NEW :- Added Login Functionality to the aplication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,51 @@
 import React from "react";
+import axios from "axios";
 
+const initialState ={
+    username: "",
+    password: "",
+}
 const Login = () => {
+    const [loginData,setLoginData] = React.useState(initialState);
+
+    const handleChange = (e) =>{
+        const {name , value} = e.target;
+        setLoginData(prev =>(
+            {...prev,[name] : value}
+        ));
+    }
+
+    const handleLogin = (e) =>{
+        Object.values(loginData).forEach(el => {
+            let isValid = true;
+            if(!el){
+                isValid = false;
+            }
+            if(!isValid){
+                return alert("Please fill all the fields");
+            }
+        })
+        axios({
+            method: "POST",
+            url: "https://masai-api-mocker.herokuapp.com/auth/login",
+            data : loginData
+        }).then(res => {
+            
+        }).catch(res =>{
+
+        })
+    }
     return(
-        <>
-            Login
-        </>
+        <div>
+            {
+            Object.keys(loginData).map(elem =>(
+                <div>
+                    <input key = {elem} onChange = {handleChange} value = {loginData[elem]} type = "text" placeholder= {elem} />
+                </div>
+            ))
+            }
+            <button onClick={handleLogin}>Login</button>
+        </div>
     )
 }
 
